f712e341b92b98e1f11e4b46e61a62d6fad5d187
JBalajiReddy <jeedipallybalajireddy@gmail.com>
2025-05-17 19:16:14 +0530

    commit small changes

--- url-shortener-sb/src/main/java/com/url/shortener/UrlShortenerSbApplication.java
+++ url-shortener-sb/src/main/java/com/url/shortener/UrlShortenerSbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UrlShortenerSbApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(UrlShortenerSbApplication.class, args);
 	}
 
